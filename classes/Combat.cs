7bf5e436c46b8d4213dc077d46f41d6aa11bb823
machine <analyticrye@gmail.com>
2020-01-05 01:18:19 -0500

    added timed attacks and health reports

--- classes/Combat.cs
+++ classes/Combat.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 //think needs to be static?
 public class Combat
 {
@@ -20,32 +21,46 @@ public class Combat
 
         Monsters selectedMonster = Beasts.GetMonsters();
         int monsterFullHealth = selectedMonster.Health;
-        Console.WriteLine($"{selectedMonster.Name} appears!");
+        Console.WriteLine($"{selectedMonster.Name} appears!\n");
         //store  
         bool isShopping = false;
         Shop shop = new Shop();
+        string storeInput = "";
         while (combatPlaying == true)
         {
-            string input = "";
+
+            //players turn
             if (playerTurn == true)
             {
-                Console.WriteLine("Player's turn");
-                Console.WriteLine($"You attack {selectedMonster.Name} for {Player.Damage} damage!");
+                Console.WriteLine("----------------------------------\n");
+                Console.WriteLine("Player's turn \n");
+                //player attack waits
+                Task.WaitAll(Attack(600));
+                Console.WriteLine($"You attack {selectedMonster.Name} for {Player.Damage} damage!\n");
                 selectedMonster.TakesDamage(Player.Damage);
-                Console.WriteLine($"{selectedMonster.Name} is left with {selectedMonster.Health} health!");
-                input = Console.ReadLine().ToLower();
-                if (input == "shop" || input == "s")
+                //wait to show health
+                Task.WaitAll(Attack(800));
+                Console.WriteLine($"{selectedMonster.Name} is left with {selectedMonster.Health} health!\n");
+                storeInput = Console.ReadLine().ToLower();
+                //go to shop
+                if (storeInput == "shop" || storeInput == "s")
                 {
                     isShopping = true;
                     shop.Shopping(isShopping);
                 }
                 playerTurn = false;
             }
+            //monsters turn
             else
             {
-                Console.WriteLine("Monster's turn");
-                Console.WriteLine($"{selectedMonster.Name} attacks you for {selectedMonster.Damage} damage!");
+                Console.WriteLine("----------------------------------\n");
+                Console.WriteLine("Monster's turn\n");
+                //monster attack waits
+                Task.WaitAll(Attack(600));
+                Console.WriteLine($"{selectedMonster.Name} attacks you for {selectedMonster.Damage} damage!\n");
                 Player.TakesDamage(selectedMonster.Damage);
+                //wait to show health
+                Task.WaitAll(Attack(800));
                 Console.WriteLine($"You have {Player.Health} health left!");
                 Console.ReadLine();
                 playerTurn = true;
@@ -65,5 +80,15 @@ public class Combat
                 combatPlaying = false;
             }
         }
+    }
+    public static async Task Attack(int duration)
+    {
+        await AttackDelay(duration);
+        //Console.WriteLine("Attacks");
+    }
+    public static async Task<int> AttackDelay(int duration)
+    {
+        await Task.Delay(duration);
+        return 1;
     }
 }
--- classes/Shop.cs
+++ classes/Shop.cs
@@ -1,12 +1,6 @@
 using System;
 public class Shop
 {
-
-    // public bool IsShoping { get; set; }
-    // public Shop(bool isShoping)
-    // {
-    //     IsShoping = isShoping;
-    // }
     public void Shopping(bool isShoping)
     {
 
